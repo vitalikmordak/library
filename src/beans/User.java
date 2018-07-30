3a6d1aa1c184003646a0b9d1097b7607014e9e8d
vitalikmordak <vitalikmordak@gmail.com>
2018-07-30 13:16:53 +0300

    add new validations in login form

--- src/beans/User.java
+++ src/beans/User.java
@@ -1,10 +1,8 @@
 package beans;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.util.Map;
 
 @Named
 @SessionScoped
--- src/validators/LoginValidator.java
+++ src/validators/LoginValidator.java
@@ -7,22 +7,39 @@ import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 import java.util.ResourceBundle;
+
 @FacesValidator("LoginValidator")
 public class LoginValidator implements Validator {
     @Override
     public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+        ResourceBundle bundle = ResourceBundle.getBundle("nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+        String value = o.toString();
+        try {
+            switch (uiComponent.getId()) {
+                case "username":
+                    if (value.length() < 5) {
+                        throw new IllegalArgumentException(bundle.getString("error_login_length"));
+                    }
+                    if (!Character.isLetter(value.charAt(0))) {
+                        throw new IllegalArgumentException(bundle.getString("error_first_letter"));
+                    }
+                    if (value.equals("username") || o.toString().equals("login")) {
+                        throw new IllegalArgumentException(bundle.getString("error_login_used").concat(o.toString() + "\""));
+                    }
 
-        if (uiComponent.getId().equals("username") && o.toString().length() < 5) {
-            ResourceBundle bundle = ResourceBundle.getBundle("nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
-            FacesMessage message = new FacesMessage(bundle.getString("error_login_length"));
+                    break;
+                case "password":
+                    if (value.length() < 5) {
+                        throw new IllegalArgumentException(bundle.getString("error_password_length"));
+                    }
+                    break;
+            }
+        } catch (IllegalArgumentException e) {
+            FacesMessage message= new FacesMessage(e.getMessage());
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(message);
         }
-        if (uiComponent.getId().equals("password") && o.toString().length() < 5) {
-            ResourceBundle bundle = ResourceBundle.getBundle("nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
-            FacesMessage message = new FacesMessage(bundle.getString("error_password_length"));
-            message.setSeverity(FacesMessage.SEVERITY_ERROR);
-            throw new ValidatorException(message);
-        }
+
+
     }
 }
