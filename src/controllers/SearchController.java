f9c86dc320b306f20b64011d371cad5797922f7f
vitalikmordak <vitalikmordak@gmail.com>
2018-07-31 16:06:51 +0300

    Create templates for header, footer, content and left menu

---
+++ src/controllers/SearchController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import enums.SearchType;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+@Named
+@ApplicationScoped
+public class SearchController {
+    private SearchType searchType;
+    private static Map<String, SearchType> searchList = new HashMap<>();
+
+    public SearchController() {
+        ResourceBundle bundle = ResourceBundle.getBundle("nls.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+        searchList.put(bundle.getString("author_enum"), SearchType.AUTHOR);
+        searchList.put(bundle.getString("book_title_enum"), SearchType.TITLE);
+    }
+
+    public SearchType getSearchType() {
+        return searchType;
+    }
+
+//    public static Map<String, SearchType> getSearchList() {
+//        return searchList;
+//    }
+
+    public Object getSearchList() {
+        return searchList;
+    }
+}
