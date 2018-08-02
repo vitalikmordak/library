055dbf3cadfac049a579c73191692e65f571bd2a
vitalikmordak <vitalikmordak@gmail.com>
2018-08-03 00:30:33 +0300

    added the ability to download pdf

--- src/servlets/PdfContent.java
+++ src/servlets/PdfContent.java
@@ -1,7 +1,9 @@
 package servlets;
 
 import beans.Book;
+import controllers.BookController;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,8 @@ import java.util.ArrayList;
 
 @WebServlet("/PdfContent")
 public class PdfContent extends HttpServlet {
-
+    @Inject
+    BookController bookController;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,10 +32,17 @@ public class PdfContent extends HttpServlet {
         resp.setContentType("application/pdf");
 
         try (OutputStream out = resp.getOutputStream()) {
-            int index = Integer.valueOf(req.getParameter("index"));
+//            int index = Integer.valueOf(req.getParameter("index"));
+            int id = Integer.parseInt(req.getParameter("id"));
 
-            ArrayList<Book> list = (ArrayList<Book>) req.getSession(false).getAttribute("currentBookList");
-            Book book = list.get(index);
+//            ArrayList<Book> list = (ArrayList<Book>) req.getSession(false).getAttribute("currentBookList");
+            ArrayList<Book> list = bookController.getBookList();
+            Book book = new Book();
+            for (Book b : list) {
+                if (b.getId() == id) {
+                    book = b;
+                }
+            }
             // Downloading pdf
             book.downloadPdf();
             resp.setContentLength(book.getContent().length);
