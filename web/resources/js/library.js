aaf6a228aabb8fce85a4d9fb121e1556b94d6f69
vitalikmordak <vitalikmordak@gmail.com>
2018-08-04 18:32:04 +0300

    add AJAX for loading content

---
+++ web/resources/js/library.js
@@ -0,0 +1,8 @@
+function loading(data) {
+    if (data.status === "begin") {
+        document.getElementById('loading_wrapper').style.display = "block";
+    } else if (data.status === "success") {
+        document.getElementById('loading_wrapper').style.display = "none";
+    }
+
+}
