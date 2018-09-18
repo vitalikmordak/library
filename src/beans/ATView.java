86dfad76bd47af3d959378446498b2dddcab12c5
vitalikmordak <vitalikmordak@gmail.com>
2018-09-18 12:51:38 +0300

    change dialog-titlebar color and reformat code

--- src/beans/ATView.java
+++ src/beans/ATView.java
@@ -12,9 +12,9 @@ import java.util.Map;
 public class ATView {
     public void editBook() {
         Map<String, Object> options = new HashMap<>();
-        options.put("resizable",false);
+        options.put("resizable", false);
         options.put("draggable", false);
-        options.put("modal",true);
-        PrimeFaces.current().dialog().openDynamic("/pages/editBook.xhtml",options,null);
+        options.put("modal", true);
+        PrimeFaces.current().dialog().openDynamic("/pages/editBook.xhtml", options, null);
     }
 }
